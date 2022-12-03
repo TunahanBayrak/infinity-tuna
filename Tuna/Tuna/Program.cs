3e8f07f3da8be30d798fe0ec8b061d12dc8fff70
tun4a <51829157+tun4a@users.noreply.github.com>
2022-12-03 22:14:14 +0300

    Add

---
+++ Tuna/Tuna/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static int i;
+
+    private static void Main(string[] args)
+    {
+        for (i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("Tuna Han Bayrak");
+        }
+}
+}
